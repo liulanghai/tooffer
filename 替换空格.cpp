18daa9ba5bc7e75f09c1c416a419b9b8d8ee66da
denghuaguang <denghuaguang@onething.net>
2019-09-20 10:34:34 +0800

    提交

--- 替换空格.cpp
+++ 替换空格.cpp
@@ -1,5 +1,5 @@
 
-#include "head.h"
+#include "common.h"
 /*
 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
@@ -7,22 +7,21 @@
 class Solution {
 public:
 	void replaceSpace(char *str,int length) {
-        char *res  = (char *)malloc(length*3);
-        int i = 0;
-        int j = 0;
-        for(i=0;i<length;i++){
-            if(str[i] == ' '){
-                printf("xxx\n");
-                res[j++] = '%';
-                res[j++] = '2';
-                res[j++] = '0';
+        char *tmp = (char *)malloc(length);
+        for(int i=0;i<length;i++){
+            tmp[i] = str[i];
+        }
+        int j=0;
+        for(int i=0;i<length;i++){
+            if(tmp[i] == ' '){
+                str[j++] = '%';
+                str[j++] = '2';
+                str[j++] = '0';
             }else{
-                res[j++] = str[i];
+                str[j++] = tmp[i];
             }
         }
-        res[j] = 0;
-        str = res;
-        printf("%s\n",str);
+        str[j] = 0;
 	}
 };
 
@@ -33,7 +32,7 @@ int main(){
    a[1] = ' ';
    a[2] = '2';
    s.replaceSpace(a,3);
-   printf("%s\n",a);
+   printf("out func %s\n",a);
    return 0;
 }
  
